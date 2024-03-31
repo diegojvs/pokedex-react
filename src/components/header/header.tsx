f42fdd750976016662ee7cbd1204baf246610bb6
Diego Vergara <diegoos.e.m@gmail.com>
2024-03-31 19:11:33 -0300

    Add header component

---
+++ src/components/header/header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header className="flex h-24 items-center justify-center bg-red-500 p-4">
+      <h1 className=" text-4xl font-black text-slate-50">Pokédex</h1>
+    </header>
+  );
+};
+
+export default Header;
