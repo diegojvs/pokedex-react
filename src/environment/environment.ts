cc371b06bb65bebb2d5d824593f947f02e8bb18e
Diego Vergara <diegoos.e.m@gmail.com>
2024-04-02 19:34:05 -0300

    Add getPokemon method to pokeApiUrls

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,6 @@
 export const pokeApiUrls = {
 	listPokemon: (offset: number, limit: number) =>
 		`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`,
+	getPokemon: (name: string | number) =>
+		`https://pokeapi.co/api/v2/pokemon/${name}`,
 };
