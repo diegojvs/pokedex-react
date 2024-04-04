2b5c4ecedba942e33b4a4e16081bd0ac5b6789ce
Diego Vergara <diegoos.e.m@gmail.com>
2024-04-03 21:03:17 -0300

    Add Loader component

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,10 @@
+const Loader = () => {
+	return (
+		<div
+			className="h-48 w-48 animate-spin rounded-full border-2 border-t-red-500"
+			role="status"
+		></div>
+	);
+};
+
+export default Loader;
