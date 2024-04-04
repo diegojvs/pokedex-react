5ce63eb1708de232d7aef2d298b33a5d995e8b1f
Diego Vergara <diegoos.e.m@gmail.com>
2024-04-03 21:03:56 -0300

    Add ContainerCardsPokemon component to Home page

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,9 +1,11 @@
 import Header from "@components/header/header.tsx";
+import ContainerCardsPokemon from "@src/components/containerCardsPokemon/containerCardsPokemon";
 
 const Home = () => {
 	return (
 		<>
 			<Header />
+			<ContainerCardsPokemon />
 		</>
 	);
 };
