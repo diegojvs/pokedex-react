cffbeb2b267c92c25f0382ab25e5f80067a35eb8
Diego Vergara <diegoos.e.m@gmail.com>
2024-04-11 20:41:08 -0400

    Refactor ContainerCardsPokemon component to use useFilter hook for filtering Pokemon

--- src/components/containerCardsPokemon/containerCardsPokemon.tsx
+++ src/components/containerCardsPokemon/containerCardsPokemon.tsx
@@ -4,6 +4,7 @@ import { genPokemon } from "@src/utils/const";
 import { Pokemon } from "@src/models/pokemon";
 import Loader from "../loader/loader";
 import CardPokemon from "../cardPokemon/cardPokemon";
+import useFilter from "@src/hooks/useFilter";
 
 const ContainerCardsPokemon = () => {
 	const [pokemon, setPokemon] = useState<Pokemon[]>([]);
@@ -11,6 +12,7 @@ const ContainerCardsPokemon = () => {
 	const [error, setError] = useState(false);
 	const pokemonServices = new PokemonServices();
 	const startAndEndGeneration = genPokemon;
+	const { filterPokemon } = useFilter();
 
 	const getPokemon = async (name: string | number) => {
 		const pokemon = await pokemonServices.getPokemon(name);
@@ -31,13 +33,32 @@ const ContainerCardsPokemon = () => {
 
 			const pokemonList: Pokemon[] = [];
 
-			for (let i = 0; i < startAndEndGeneration[0].end; i++) {
-				const pokemon = await getPokemon(listPokemon.results[i].name);
+			// create array of n numbers from 0 to startAndEndGeneration[0].end
+
+			const numbers = Array.from(
+				{
+					length:
+						startAndEndGeneration[0].end - startAndEndGeneration[0].start + 1,
+				},
+				(_, index) => index + startAndEndGeneration[0].start,
+			);
+
+			const response = await Promise.all(
+				numbers.map((number) => {
+					return getPokemon(number);
+				}),
+			);
+
+			if (!response) {
+				return;
+			}
+
+			response.forEach((pokemon) => {
 				if (!pokemon) {
 					return;
 				}
 				pokemonList.push(pokemon);
-			}
+			});
 
 			setPokemon(pokemonList);
 			setLoading(false);
@@ -51,14 +72,14 @@ const ContainerCardsPokemon = () => {
 	}, []);
 
 	return (
-		<div className="mx-auto flex min-h-dvh w-[80%] items-center justify-center">
+		<div className="mx-auto flex min-h-dvh w-[80%] items-center justify-center pt-24">
 			{loading ? (
 				<div className="flex h-full w-full items-center justify-center">
 					<Loader />
 				</div>
 			) : (
 				<div className="grid grid-cols-1 gap-8 p-4 md:grid-cols-2 lg:grid-cols-3">
-					{pokemon.map((pokemon) => (
+					{filterPokemon(pokemon).map((pokemon) => (
 						<CardPokemon key={pokemon.id} {...pokemon} />
 					))}
 				</div>
