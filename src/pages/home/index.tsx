e3f03e15d452a9e55258352f68444dadd275447d
Diego Vergara <diegoos.e.m@gmail.com>
2024-04-15 18:55:42 -0400

    Refactor Home component to use PokemonProvider for managing Pokemon state

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,14 +1,17 @@
 import Header from "@components/header/header.tsx";
 import ContainerCardsPokemon from "@src/components/containerCardsPokemon/containerCardsPokemon";
 import { FilterProvider } from "@src/context/filters";
+import { PokemonProvider } from "@src/context/pokemon";
 
 const Home = () => {
 	return (
 		<>
-			<FilterProvider>
-				<Header />
-				<ContainerCardsPokemon />
-			</FilterProvider>
+			<PokemonProvider>
+				<FilterProvider>
+					<Header />
+					<ContainerCardsPokemon />
+				</FilterProvider>
+			</PokemonProvider>
 		</>
 	);
 };
