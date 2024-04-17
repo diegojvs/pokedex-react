365aeee0ce37a7b07c4a5663f9cc0883e05c00d7
Diego Vergara <diegoos.e.m@gmail.com>
2024-04-16 20:54:20 -0400

    Add link to home and hide filter on pokemon page

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,12 +1,16 @@
 import Filter from "../filter/filter";
+import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<header className="fixed z-10 flex h-24 w-full items-center justify-between bg-red-500 p-4 md:justify-center">
-			<h1 className="text-center text-4xl font-black text-slate-50 md:grow">
-				Pokédex
-			</h1>
-			<Filter />
+		<header className="fixed z-10 flex min-h-24 w-full flex-wrap items-center justify-between bg-red-500 p-4 md:justify-center">
+			<Link
+				to={"/"}
+				className="text-center text-4xl font-black text-slate-50 md:grow"
+			>
+				<h1>Pokédex</h1>
+			</Link>
+			{useLocation().pathname === "/" ? <Filter /> : null}
 		</header>
 	);
 };
