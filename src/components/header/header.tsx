d1d5fd88ab6f112f718681deb5078e492722ebb8
Diego Vergara <diegoos.e.m@gmail.com>
2024-04-29 22:06:07 -0400

    Update header styling in header.tsx

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -3,7 +3,9 @@ import { Link, useLocation } from "react-router-dom";
 
 const Header = () => {
 	return (
-		<header className="fixed z-10 flex min-h-24 w-full flex-wrap items-center justify-between bg-red-500 p-4 md:justify-center">
+		<header
+			className={`fixed z-10 flex min-h-24 w-full flex-wrap items-center bg-red-500 p-4 md:justify-center ${useLocation().pathname === "/" ? "justify-between" : "justify-center"}`}
+		>
 			<Link
 				to={"/"}
 				className="text-center text-4xl font-black text-slate-50 md:grow"
