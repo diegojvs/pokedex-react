9f590f5a174839e68123c907628b5803aabbae91
Diego Vergara <diegoos.e.m@gmail.com>
2024-04-29 22:04:23 -0400

    Add new methods for retrieving species and evolution chain

--- src/services/pokemonServices.ts
+++ src/services/pokemonServices.ts
@@ -1,6 +1,8 @@
-import { listPokemon } from "@src/models/listPokemon";
-import { Pokemon } from "@src/models/pokemon";
+import { listPokemon } from "@models/listPokemon";
+import { Pokemon } from "@models/pokemon";
 import { pokeApiUrls } from "@src/environment/environment";
+import { Species } from "@models/species";
+import { EvolutionChain } from "@models/evolutionChain";
 
 export class PokemonServices {
 	constructor() {}
@@ -20,6 +22,33 @@ export class PokemonServices {
 	async getPokemon(name: string | number): Promise<Pokemon | undefined> {
 		try {
 			const response = await fetch(pokeApiUrls.getPokemon(name));
+
+			if (response.status === 404) return;
+
+			return response.json();
+		} catch (error) {
+			console.error("Error: ", error);
+		}
+	}
+
+	async getSpecies(url: string): Promise<Species | undefined> {
+		try {
+			const response = await fetch(url);
+
+			if (response.status === 404) return;
+
+			return response.json();
+		} catch (error) {
+			console.error("Error: ", error);
+		}
+	}
+
+	async getEvolutionChain(url: string): Promise<EvolutionChain | undefined> {
+		try {
+			const response = await fetch(url);
+
+			if (response.status === 404) return;
+
 			return response.json();
 		} catch (error) {
 			console.error("Error: ", error);
